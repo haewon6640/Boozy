e2061068293a9835ecd9985d1b21c5f74ad3067b
jeremy <jeremypietila@gmail.com>
2022-06-01 16:44:15 -0700

    add geolocation

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 // import 'dotenv/config' 
 
 class BoozyMap extends React.Component {
@@ -12,9 +12,30 @@ class BoozyMap extends React.Component {
     //             lat: lat,
     //             lng: lng
     //   }
+    }
 
-    // }
-}
+    componentDidMount() {
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+          };
+          
+          function success(pos) {
+            var crd = pos.coords;
+          
+            console.log('Your current position is:');
+            console.log(`Latitude : ${crd.latitude}`);
+            console.log(`Longitude: ${crd.longitude}`);
+            console.log(`More or less ${crd.accuracy} meters.`);
+          }
+          
+          function error(err) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+          }
+          
+          navigator.geolocation.getCurrentPosition(success, error, options);
+    }
   
 
   
@@ -30,8 +51,21 @@ class BoozyMap extends React.Component {
                 {
                     lat: 37.66385274501688,
                     lng: -122.4351120016871
-                }
-            } />
+                }}
+                onClick={this.onMapClicked}
+            >
+
+            <Marker
+                title={'sooner or later'}
+                name={'????'}
+                position={{lat: 37.778519, lng: -122.405640}} />
+                  <InfoWindow>
+                    <div className="infowindow">
+                        <p>Click on the map or drag the marker to select location where the incident occurred</p>
+                    </div>
+                </InfoWindow>
+
+            </Map>
         </div>
       )
     }
