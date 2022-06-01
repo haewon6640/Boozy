2fdab15419d49b117d70ea27e294851a5b9a5b12
Haewon6640 <haewon6640@gmail.com>
2022-06-01 12:08:43 -0700

    add category to ingredient

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -5,6 +5,10 @@ const IngredientSchema = new Schema({
     name: {
         type: String,
         required: true
+    },
+    category: {
+        type: String, 
+        required: true
     }
 });
 
