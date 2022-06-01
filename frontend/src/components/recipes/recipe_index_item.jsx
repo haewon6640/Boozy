5fd0f921432d3b3f94e3d8ccab97b207e955ccc2
Haewon6640 <haewon6640@gmail.com>
2022-06-01 09:30:17 -0700

    recipe show backend connect

--- frontend/src/components/recipes/recipe_index_item.jsx
+++ frontend/src/components/recipes/recipe_index_item.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const RecipeIndexItem = (props) => {
     return (
-        <li className="recipe-index-item">
-			<div className="photo-box">
-				<img src="https://www.yummymummykitchen.com/wp-content/uploads/2019/06/greyhound-drink-cocktail-10-720x720.jpg" 
-				alt={props.recipe.name}
-				/>
-			</div>
-            <div className="photo-title">{props.recipe.name}</div>
-            {/* <p>{props.recipe.created_at}</p> */}
-            {/* {props.recipe.ingredients.map(ingredient_id=>(
-                <span>{props.ingredients[ingredient_id].name}</span>
-            ))} */}
-        </li>
+        <Link to={`/recipes/${props.key}`}>
+            <li className="recipe-index-item">
+                <div className="photo-box">
+                    <img src="https://www.yummymummykitchen.com/wp-content/uploads/2019/06/greyhound-drink-cocktail-10-720x720.jpg" 
+                    alt={props.recipe.name}
+                    />
+                </div>
+                <div className="photo-title">{props.recipe.name}</div>
+                {/* <p>{props.recipe.created_at}</p> */}
+                {/* {props.recipe.ingredients.map(ingredient_id=>(
+                    <span>{props.ingredients[ingredient_id].name}</span>
+                ))} */}
+            </li>
+        </Link>
     )
 }
 export default RecipeIndexItem;
