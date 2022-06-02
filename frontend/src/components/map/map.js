6f0d19b864cf954feb9f406aad2db4f8a8d6d1a5
jeremy <jeremypietila@gmail.com>
2022-06-02 09:10:40 -0700

    map geolocation

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -7,64 +7,98 @@ class BoozyMap extends React.Component {
         super(props)
     //     const {lat, lng} = this.props.initialCenter;
 
-    //     this.state = {
-    //         currentLocation: {
-    //             lat: lat,
-    //             lng: lng
-    //   }
+        this.state = {
+            center: { lat: 0, lng: 0},
+            coordsResult: [],
+            trigger: 0
+        }
+        this.setState = this.setState.bind(this)
     }
 
     componentDidMount() {
+
         var options = {
             enableHighAccuracy: true,
             timeout: 5000,
             maximumAge: 0
-          };
-          
-          function success(pos) {
+        };
+
+                      
+        let success = function success(pos) {
             var crd = pos.coords;
-          
+            // debugger
             console.log('Your current position is:');
             console.log(`Latitude : ${crd.latitude}`);
             console.log(`Longitude: ${crd.longitude}`);
             console.log(`More or less ${crd.accuracy} meters.`);
-          }
-          
-          function error(err) {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
-          }
-          
-          navigator.geolocation.getCurrentPosition(success, error, options);
+            
+            this.setState({
+                center: { lat: crd.latitude, lng: crd.longitude},
+                trigger: 1
+
+            })
+        }.bind(this)
+        
+        function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+
+       
+        
+        navigator.geolocation.getCurrentPosition(success, error, options);
     }
+
+    onMapLoad = map => {
+        let coords= {}
+        let request = {
+          query: "Safeway",
+          fields: ["name", "geometry"]
+        };
+    
+        let service = new window.google.maps.places.PlacesService(map);
+    
+        service.findPlaceFromQuery(request, (results, status) => {
+          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              coords.push(results[i]);
+            }
+    
+            this.setState({
+            //   center: results[0].geometry.location,
+              coordsResult: coords
+            });
+          }
+        });
+      };
   
 
   
     render() {
-
+        console.log(this.state)
       return (
         <div className="map-component">
             <Map
                 classname="map-component"
                 google={this.props.google}
-                zoom={8}
-                initialCenter={
-                {
-                    lat: 37.66385274501688,
-                    lng: -122.4351120016871
-                }}
+                zoom={14}
+                initialCenter={this.state.center}
                 onClick={this.onMapClicked}
+                onLoad={map => this.onMapLoad(map)}
             >
-
-            <Marker
-                title={'sooner or later'}
-                name={'????'}
-                position={{lat: 37.778519, lng: -122.405640}} />
-                  <InfoWindow>
-                    <div className="infowindow">
-                        <p>Click on the map or drag the marker to select location where the incident occurred</p>
-                    </div>
-                </InfoWindow>
-
+                {this.state.coordsResult !== [] &&
+                    this.state.coordsResult.map(function(results, i) {
+                        return (
+                            <Marker key={i} position={results.geometry.location}>
+                            <InfoWindow 
+                            options={{ maxWidth: 300 }}>
+                                
+                                <span>{results.name}</span>
+                                
+                            </InfoWindow>
+                            </Marker>
+                        )
+                    }
+              )}
             </Map>
         </div>
       )
