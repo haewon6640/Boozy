2d7084e706b81c4c26b9cf446a12298945789171
Pamm-j <pamela.palomita@gmail.com>
2022-06-02 14:07:33 -0700

    added in-line class

--- frontend/src/components/profile/filter_choice.jsx
+++ frontend/src/components/profile/filter_choice.jsx
@@ -4,55 +4,69 @@ export default class FilterChoice extends Component {
   render() {
     return (
       	<div className="radio-container">
-			<label>Boozy
-				<input type="radio" 
-					name="filter_choice"
-					style={{ accentColor: "#EF6079FF" }}
-					onChange={()=>this.props.handleSelection('boozy')}
-					/>
-			</label>
-			<label>Sweet
-				<input type="radio" 
-					name="filter_choice"
-					style={{ accentColor: "#EF6079FF" }}
-					onChange={()=>this.props.handleSelection('sweet')}
-					/>
-			</label>
-			<label>Sour
-				<input type="radio" 
-					name="filter_choice"
-					style={{ accentColor: "#EF6079FF" }}
-					onChange={()=>this.props.handleSelection('sour')}
-					/>
-			</label>
-			<label>Bitter
-				<input type="radio" 
-					name="filter_choice"
-					style={{ accentColor: "#EF6079FF" }}
-					onChange={()=>this.props.handleSelection('bitter')}
-					/>
-			</label>
-			<label>Salty
-				<input type="radio" 
-					name="filter_choice"
-					style={{ accentColor: "#EF6079FF" }}
-					onChange={()=>this.props.handleSelection('salty')}
-					/>
-			</label>
-			<label>Umami
-				<input type="radio" 
-					name="filter_choice"
-					style={{ accentColor: "#EF6079FF" }}
-					onChange={()=>this.props.handleSelection('umami')}
-					/>
-			</label>
-			<label>High Rating
-				<input type="radio" 
-					name="filter_choice"
-					style={{ accentColor: "#EF6079FF" }}
-					onChange={()=>this.props.handleSelection('rating')}
-					/>
-			</label>
+          <div className='in-line'>
+            <label>Boozy
+              <input type="radio" 
+                name="filter_choice"
+                style={{ accentColor: "#EF6079FF" }}
+                onChange={()=>this.props.handleSelection('boozy')}
+                />
+            </label>
+          </div>
+        <div className='in-line'>     
+          <label>Sweet
+            <input type="radio" 
+              name="filter_choice"
+              style={{ accentColor: "#EF6079FF" }}
+              onChange={()=>this.props.handleSelection('sweet')}
+              />
+          </label>
+        </div>
+        <div className='in-line'>
+          <label>Sour
+            <input type="radio" 
+              name="filter_choice"
+              style={{ accentColor: "#EF6079FF" }}
+              onChange={()=>this.props.handleSelection('sour')}
+              />
+          </label>
+        </div>
+        <div className='in-line'>
+          <label>Bitter
+            <input type="radio" 
+              name="filter_choice"
+              style={{ accentColor: "#EF6079FF" }}
+              onChange={()=>this.props.handleSelection('bitter')}
+              />
+          </label>
+        </div>
+        <div className='in-line'>
+          <label>Salty
+            <input type="radio" 
+              name="filter_choice"
+              style={{ accentColor: "#EF6079FF" }}
+              onChange={()=>this.props.handleSelection('salty')}
+              />
+          </label>
+        </div>
+        <div className='in-line'>
+          <label>Umami
+            <input type="radio" 
+              name="filter_choice"
+              style={{ accentColor: "#EF6079FF" }}
+              onChange={()=>this.props.handleSelection('umami')}
+              />
+          </label>
+        </div>
+        <div className='in-line'>
+          <label>High Rating
+            <input type="radio" 
+              name="filter_choice"
+              style={{ accentColor: "#EF6079FF" }}
+              onChange={()=>this.props.handleSelection('rating')}
+              />
+          </label>
+        </div>
 		</div>
     )
   }
