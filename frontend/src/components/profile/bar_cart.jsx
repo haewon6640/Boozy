e2a7b337b15c81b9351d0e961a98b4576bcfdea2
pam <pamela.palomita@gmail.com>
2022-06-04 22:00:08 -0700

    rewrote classes as functional components to use hooks

--- frontend/src/components/profile/bar_cart.jsx
+++ frontend/src/components/profile/bar_cart.jsx
@@ -1,65 +1,95 @@
 import React from "react";
 import Autocomplete from "./auto_complete";
 import CanMake from "./can_make";
-import CanMaybeMake from "./can_mayby_make";
-import {AiOutlineCloseCircle} from 'react-icons/ai'; 
+import CanMaybeMake from "./can_maybe_make";
 import { IoIosArrowBack, IoIosArrowDown } from 'react-icons/io';
 import BarCartRecipeShow from "./barcart_recipe_show";
 import FilterChoice from "./filter_choice";
-import { fetchUser } from "../../actions/user_actions";
+import Shelf from "./shelf";
 class BarCart extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            missing: [],
-            barcart_open: true,
+			// booleans to control accordian
+			barcart_open: true,
             filter_open: false,
             can_open: true,
             cant_open: true,
+			// to controle what is seen in recipe show
             curr_recipe: {},
-            curr_ingredients: [],
+            // curr_ingredients: [],
+            // missing: [],
             user: {},
-            first:true,
-			      filter_choice:""
+			filter_choice:"",
+			can_make:[]
         };
         this.addItem = this.addItem.bind(this);
-        this.handleHover = this.handleHover.bind(this);
+        // this.showRecipe = this.showRecipe.bind(this);
         this.toggleBarCart = this.toggleBarCart.bind(this);
-        this.getNeededIngredients = this.getNeededIngredients.bind(this);
-		this.handleSelection = this.handleSelection.bind(this);
-		this.autoPopulate = this.autoPopulate.bind(this)
+        // this.getNeededIngredients = this.getNeededIngredients.bind(this);
+        this.handleSelection = this.handleSelection.bind(this);
+        this.autoPopulate = this.autoPopulate.bind(this)
+		this.findCanDrinks = this.findCanDrinks.bind(this)
     }
     componentDidMount() {
         this.props.fetchIngredients();
         this.props.fetchRecipes();
         this.props.fetchUser()
             .then(()=>this.setState({user: this.props.user}));
+		this.findCanDrinks()
+		this.autoPopulate()
     }
-    getNeededIngredients(ingredientList) {
-		if (ingredientList) {
-			return Object.values(this.props.ingredients).filter(ing=>
-				ingredientList.includes(ing._id))
-		}
+    // getNeededIngredients(ingredientList) {
+    // 	// if (ingredientList) {
+    //     // 	return Object.values(this.props.ingredients).filter(ing=>
+    //     //   	ingredientList.includes(ing._id))
+    // 	// }
+	// }
 
-		}
+	componentDidUpdate(prevProps){
+		console.log(prevProps)
+		// if(!prevProps.user) {
+		// 	this.props.fetchIngredients();
+		// 	this.props.fetchRecipes();
+		// 	this.props.fetchUser()
+		// 		.then(()=>this.setState({user: this.props.user}));
+		// 	this.findCanDrinks()
+		// 	this.autoPopulate()
+		// }
+	}
 
-    autoPopulate(data){
-		if (data && this.state.first && Object.values(this.state.curr_recipe).length === 0){
-			this.handleHover(data.recipe, [], data.ingredients)
-		}
+	findCanDrinks = ()=>{   
+		let canMake = Object.values(this.props.recipes.all).filter(recipe=>(
+			recipe.ingredients.every(ingredient=>(
+				this.props.user.shelf.includes(ingredient)|| ingredient === null))) &&
+				(recipe.avg_rating[this.state.filter_choice] >= 3 || this.state.filter_choice === "")
+		)
+		this.setState({can_make:canMake})
+		console.log() // don't delete this
+	}
 
+    autoPopulate(){
+		if (
+			this.state.can_make.length > 0 && 
+			Object.values(this.state.curr_recipe).length === 0
+		){
+			this.setState({
+				first:false, 
+				curr_recipe:this.state.can_make[0]
+			})
+		}
     }
 
-    handleHover(recipe, missing, ingredients) {
-        this.setState({ 
-			curr_recipe: recipe, 
-			missing: this.getNeededIngredients(missing, missing), 
-			curr_ingredients: this.getNeededIngredients(ingredients, missing) 
-		});
-    }
+    // showRecipe(recipe, missing, ingredients) {
+    //     this.setState({ 
+	// 		curr_recipe: recipe, 
+	// 		missing: this.getNeededIngredients(missing, missing), 
+	// 		curr_ingredients: this.getNeededIngredients(ingredients, missing) 
+	// 	});
+    // }
 
-	handleSelection(value){
-		this.setState({filter_choice:value})
+	handleSelection(type, value){
+		this.setState({[type]:value})
 	}
 
     addItem(item) {
@@ -79,44 +109,32 @@ class BarCart extends React.Component {
         }
         let barcart = "";
         const dictionary = Object.values(this.props.ingredients);
-        // if (!this.state.barcart_open) {
-        //     barcart = (
-        //         <button onClick={()=>this.toggleBarCart("barcart_open")} className="btn btn-barcart">
-        //             Open Barcart
-        //         </button>
-        //     )
-        // } else {
-          console.log(this.props)
-            barcart = (
-                <div className="cart-box">
-					<div className="in-line" onClick={()=>this.toggleBarCart("barcart_open")}>
-						<h2> Your Bar Cart</h2>
-						{!this.state.barcart_open && <IoIosArrowBack className='arrow'/>}
-            			{this.state.barcart_open && <IoIosArrowDown className='arrow'/>}
-					</div>
-					{ this.state.barcart_open && <div>
-						<Autocomplete
-							className="search-input-web"
-							dictionary={dictionary}
-							addItem={this.addItem}
-						/>
-						<ul>
-							{this.props.user.shelf !== [] &&
-								this.props.user.shelf.map((item) => (
-									<li className="bar-items" key={item}>
-										{<h3>{this.props.ingredients[item].name}</h3>}
-										<AiOutlineCloseCircle 
-											onClick={()=>this.props.deleteFromShelf(item, this.props.user.id)
-												.then(()=>this.props.fetchUser())}
-											className="remove"
-										/>
-									</li>
-								))}
-						</ul>
-					</div>}
+        barcart = (
+            <div className="cart-box">
+                <div className="in-line" onClick={()=>this.toggleBarCart("barcart_open")}>
+                    <h2> Your Bar Cart</h2>
+                    {!this.state.barcart_open && <IoIosArrowBack className='arrow'/>}
+                    {this.state.barcart_open && <IoIosArrowDown className='arrow'/>}
                 </div>
-            )
-        // }
+                {this.state.barcart_open && <div>
+                    <Autocomplete
+                        className="search-input-web"
+                        dictionary={dictionary}
+                        addItem={this.addItem}
+					/>
+					{this.props.user.shelf !== [] && 
+					<Shelf
+						user={this.props.user}
+						ingredients={this.props.ingredients}
+						remove={this.props.deleteFromShelf}
+						fetchUser={this.props.fetchUser}
+						findCanDrinks={this.findCanDrinks}
+					/>}
+
+                </div>}
+            </div>
+        )
+
         return (
             <div className="webpage">
                 <div className="two-col asym">
@@ -129,30 +147,29 @@ class BarCart extends React.Component {
 								filter_open={this.state.filter_open}
 							/>
                             <CanMake
-								filter_choice={this.state.filter_choice}
-                                recipes={this.props.recipes.all}
-                                shelf={this.props.user.shelf}
-                                handleHover={this.handleHover}
-								toggleBarCart={this.toggleBarCart}
+								handleSelection={this.handleSelection}
 								open={this.state.can_open}
 								autoPopulate={this.autoPopulate}
+								drinks={this.state.can_make}
+								toggleBarCart={this.toggleBarCart}
                             />
                             <CanMaybeMake
 								filter_choice={this.state.filter_choice}
                                 recipes={this.props.recipes.all}
                                 shelf={this.props.user.shelf}
-                                handleHover={this.handleHover}
 								toggleBarCart={this.toggleBarCart}
 								open={this.state.cant_open}
+								handleSelection={this.handleSelection}
                             />
                         </div>
                     </div>
                     <div className="bar-right">
-                        {/* {console.log(this.state.curr_recipe)} */}
                         <BarCartRecipeShow 
 							recipe={this.state.curr_recipe}
-                            ingredients={this.state.curr_ingredients}
-                            missing={this.state.missing} 
+							shelf={this.state.user.shelf}
+							showRecipe={this.showRecipe}
+							ingredients={this.props.ingredients}
+							handleSelection={this.handleSelection}
 						/>
                     </div>
                 </div>
--- frontend/src/components/profile/barcart_recipe_show.jsx
+++ frontend/src/components/profile/barcart_recipe_show.jsx
@@ -1,88 +1,86 @@
-import React from "react";
-import { GiOrangeSlice, GiTrophiesShelf } from 'react-icons/gi';
-import Boozymap from '../map/map'
+import React, {useEffect, useState} from "react";
+import { GiOrangeSlice } from 'react-icons/gi';
 import BoozyMap2 from '../map/map2'
 import ReviewGraphic from "../reviews/review_graphic";
 
-class BarCartRecipeShow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            recipe: this.props.recipe,
-            ingredients: this.props.ingredients,
-            missing: this.props.missing,
-            selectedMissing: ""
-        }
-        this.setMissing = this.setMissing.bind(this)
+export default function BarCartRecipeShow(props) {
+  const recipe = props.recipe;
+  useEffect(()=> props.handleSelection('curr_recipe', recipe), [props.shelf])
 
+  const[selectedMissing, setSelectedMissing] = useState("");
+  if (Object.values(recipe).length === 0) {
+    return null;
+  }
+  let ingredients_we_have=[];
+  let missingIngredients=[];
+  recipe.ingredients.forEach((el)=> {
+    if (props.shelf.includes(el)) {
+      ingredients_we_have.push(props.ingredients[el])
+    } else {
+      missingIngredients.push(props.ingredients[el])
     }
+  })
 
-    setMissing(missingIngredient) {
-        return e => this.setState({
-            selectedMissing: missingIngredient
-        });
-        
-    }
+  if (ingredients_we_have.length === 0){
+    return null;
+  }
 
-    render() {
-      let ingredients_we_have;
-		  if(this.props.missing) ingredients_we_have = this.props.ingredients.filter((el)=>(!this.props.missing.includes(el)))
+  console.log("lookie lookie")
+  console.log(props.shelf.length)
 
-        const recipe = this.props.recipe;
-        if (Object.values(recipe).length === 0) {
-            return null;
-        }
-        // console.log(recipe.avg_rating);
-		return (
-		<div className="barcart-recipe-show">
-			<div className="two-col">
-				<div className="recipe-left">
-                    <h1 className="recipe-title">{recipe.name}</h1>
-					<div className="show-photo">
-						<img src={recipe.imgUrl} alt="" />
-					</div>					
-				</div>
-				<div className="recipe-right">
-					<div className="two-col a-asym">
-                    <div className="recipe-ingredients">
-                        <h2>Ingredients</h2>
-                        <ul>
-                            {ingredients_we_have.map((ingredient)=>(
-                                <li key={ingredient._id}>
-                                    <GiOrangeSlice className="orange"/> 
-                                    <p>{ingredient.name}</p>
-                                </li>
-                            ))}
-                            {this.props.missing.length > 0 && <div>
-                                <p className="missing-title">--Missing Ingredients--</p>
-                                {this.props.missing.map((ingredient)=>(
-                                    <li onClick={this.setMissing(ingredient.name)} className="missing-ingredient" key={ingredient._id}>
-                                        <GiOrangeSlice className="orange"/> 
-                                        <p>{ingredient.name}</p>
-                                    </li>
-                                ))}
-                            </div>}
-                        </ul>
-					</div>
-					<div className="review-graphic">
-						<ReviewGraphic flavor_profile={recipe.avg_rating}/>
-					</div>
+  return (
+    <div className="barcart-recipe-show">
+    <div className="two-col">
+      <div className="recipe-left">
+        <h1 className="recipe-title">{recipe.name}</h1>
+        <div className="show-photo">
+          {/* <img src={recipe.imgUrl} alt="" /> */}
+        </div>					
+      </div>
 
-					</div>
-                    <div className="recipe-bottom-left">
-                       <p className="recipe-description">{recipe.description ? recipe.description : "A classical drink with hint of sweet and bitterness."}</p>
-                    </div>	
-                    <div className="recipe-steps">
-						<h2>Steps</h2>
-                        <ol>
-                            {this.props.recipe.instructions.split("\n").map((step,idx)=> (<li key={idx}>{step}</li>))}
-                        </ol>
-                    </div>
-				</div>
-			</div>
-            {this.props.missing.length > 0 && <BoozyMap2 query={this.state.selectedMissing}/>}
-		</div>
-	)}
+      <div className="recipe-right">
+        <div className="two-col a-asym">
+          <div className="recipe-ingredients">
+            <h2>Ingredients</h2>
+            <ul>
+              {ingredients_we_have.map((ingredient)=>(
+                <li key={ingredient._id}>
+                <GiOrangeSlice className="orange"/> 
+                <p>{ingredient.name}</p>
+                </li>
+              ))}
+              {missingIngredients.length > 0 && <div>
+                  <p className="missing-title">--missing--</p>
+                  {missingIngredients.map((ingredient)=>(
+                    <li className="missing-ingredient" key={ingredient._id} onClick={setSelectedMissing(ingredient)}>
+                        <GiOrangeSlice className="orange"/> 
+                        <p>{ingredient.name}</p>
+                    </li>
+                  ))}
+              </div>}
+          </ul>
+        </div>
+        <ReviewGraphic className="review-graphic" flavor_profile={recipe.avg_rating}/>
+      </div>
+        <div className="recipe-bottom-left">
+          <p className="recipe-description">This is 150 characters. This is how long it is. Look! Isn't it cool! Cocktails and puppies and clean the house. Cocktails and puppies and clean house!.</p>
+          {/* <p className="recipe-description">{recipe.description ? recipe.description : "A classical drink with hint of sweet and bitterness."}</p> */}
+        </div>	
+        <div className="recipe-steps">
+          <h2>Steps</h2>
+          <ol>
+            {props.recipe.instructions.split("\n").map((step,idx)=> (<li key={idx}>{step}</li>))}
+          </ol>
+        </div>
+      </div>
+    </div>
+          {missingIngredients.length > 0 && <BoozyMap2 query={selectedMissing}/>}
+  </div>
+  )
 }
-
-export default BarCartRecipeShow;
+// setMissing(missingIngredient) {
+//   return e => this.setState({
+//       selectedMissing: missingIngredient
+//   });
+  
+// }
